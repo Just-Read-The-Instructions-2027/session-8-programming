7d3a02e0adf52ae7c622ccdcbe3e29c01ea0a974
20175576 <20175576@tafe.wa.edu.au>
2026-09-09 11:59:39 +0800

    update weather.py

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+import math
 
 series_titles = ["Maximum temperature (Degree C)", "Minimum temperature (Degree C)", "Rainfall amount (millimetres)"]
 
@@ -7,8 +8,8 @@ def mean(in_series):
 def variance(in_series):
     pass
 
-def standard_deviation(in_series):
-    pass
+def standard_deviation(variance):
+    return math.sqrt(variance)
 
 def filter_series(year_series, month_series, day_series, data_series, max_date=None, min_date=None):
     pass
